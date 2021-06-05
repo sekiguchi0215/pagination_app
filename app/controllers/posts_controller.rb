37e78ab6ef4fc24b44a92f885924176d0d02e36e
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-06 00:40:40 +0900

    コントローラにメソッドを記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,17 @@
 class PostsController < ApplicationController
+  PER_PAGE =10
+
   def index
-    @Posts = Post.order(id: "DESC")
+    @Posts = Post.order(id: "DESC").page(params[:page]).pre(PER_PAGE)
   end
 
   def create
+    post = Post.create!(post_params)
+  end
+
+  private
+
+  def post_params
+    params.requiere(:post).permit(:body)
   end
 end
