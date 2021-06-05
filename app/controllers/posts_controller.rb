3a379514d02ee5aed3e2e7a3ebf2c28b8582f6e5
sekiguchi0215 <dsphere5296@gmail.com>
2021-06-06 00:59:37 +0900

    エラー部分の修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,17 +2,18 @@ class PostsController < ApplicationController
   PER_PAGE =10
 
   def index
+    @post = Post.new
     @posts = Post.order(id: "DESC").page(params[:page]).per(PER_PAGE)
   end
 
   def create
-    @post = Post.create!(post_params)
+    Post.create!(post_params)
     redirect_to root_path
   end
 
   private
 
   def post_params
-    params.requiere(:post).permit(:body)
+    params.require(:post).permit(:body)
   end
 end
